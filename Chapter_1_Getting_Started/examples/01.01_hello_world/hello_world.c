de09fa30bfa63193c0a0dcad916d5ee85f15b61d
whoamih4csor <122777930+whoamih4csor@users.noreply.github.com>
2023-03-07 15:31:42 -0400

    add first example

---
+++ Chapter_1_Getting_Started/examples/01.01_hello_world/hello_world.c
@@ -0,0 +1,19 @@
+/*
+AUTHOR: whoamih4csor
+DATE: tuesday, 7 march 2023
+SECTION: 1.1 Getting Started
+
+This C program is a simple example that prints the words "Hello World".
+The only way to learn a new programming language is by writing programs in it.
+
+to compile:
+gcc -ansi hello_world.c
+*/
+
+
+#include <stdio.h>/*The C library that include I/O operations*/
+
+main(){       /*this is the entry point*/
+
+    printf("Hello World!\n"); /*this function prints strings to the standar output -> terminal*/
+}
