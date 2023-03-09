215ce10d433097df18c307a86cff005b26876c93
whoamih4csor <122777930+whoamih4csor@users.noreply.github.com>
2023-03-09 06:04:38 -0400

    fix comment

--- Chapter_1_Getting_Started/exercises/01.04/01_04.c
+++ Chapter_1_Getting_Started/exercises/01.04/01_04.c
@@ -2,8 +2,7 @@
 AUTHOR: whoamih4csor
 DATE: Thursday, March 9, 2023
 
-Exercise 1-3. Modify the temperature conversion program to print a heading
-above the table
+Exercise 1-4. Write a program to print the corresponding Celsius to Fahrenheit table.
 
 print Celsius-Fahrenheit table
     for cel = 0,20,....,300
